cf06a8043548e447b62cebcf048cfa8070afaddd
william.antunes <william.antunes@invillia.com>
2022-01-30 16:54:56 -0300

    feat: ordering core implementation

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,21 +1,15 @@
-using Discount.Grpc.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Discount.Grpc
+namespace Ordering.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args)
-                .Build()
-                .MigrateDatabase<Program>()
-                .Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static Ordering.Application.Contracts.Persistence.IAsyncRepository;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
