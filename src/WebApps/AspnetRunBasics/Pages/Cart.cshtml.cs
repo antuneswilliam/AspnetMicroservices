15001b62f05c3e1411c2eb4754a49fa70ea14f36
william.antunes <william.antunes@invillia.com>
2022-02-03 09:17:36 -0300

    feat: web app

---
+++ src/WebApps/AspnetRunBasics/Pages/Cart.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspnetRunBasics
+{
+    public class CartModel : PageModel
+    {
+        private readonly IBasketService _basketService;
+
+        public CartModel(IBasketService basketService)
+        {
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+        }
+
+        public BasketModel Cart { get; set; } = new BasketModel();        
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Cart = await _basketService.GetBasket("will");            
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostRemoveToCartAsync(string productId)
+        {
+            var basket = await _basketService.GetBasket("will");
+
+            basket.Items.RemoveAll(x => x.ProductId == productId);
+
+            var basketUpdated = await _basketService.UpdateBasket(basket);
+            
+            return RedirectToPage();
+        }
+    }
+}
